6c55ce278634df479a5140dc82146601575a5f87
Mallick10 <souvikmaster1099@gmail.com>
2020-08-03 00:22:53 +0530

    Updates

---
+++ precache-manifest.1c4ac96fd495325765a6b8a9828c53b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4663bdc56d8f1c5b2c937a3dde9eeb4",
+    "url": "/Mallick10.github.io/index.html"
+  },
+  {
+    "revision": "26ac7bfff99ce6fbacfb",
+    "url": "/Mallick10.github.io/static/css/main.c5958d89.chunk.css"
+  },
+  {
+    "revision": "8de609a507c908376bcb",
+    "url": "/Mallick10.github.io/static/js/2.11a538de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mallick10.github.io/static/js/2.11a538de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ac7bfff99ce6fbacfb",
+    "url": "/Mallick10.github.io/static/js/main.97a1b98b.chunk.js"
+  },
+  {
+    "revision": "69b11e8da1d1c0a3fba9",
+    "url": "/Mallick10.github.io/static/js/runtime-main.62745f0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mallick10.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
